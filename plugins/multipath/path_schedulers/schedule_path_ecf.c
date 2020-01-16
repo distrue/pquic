c87e3d0e5365a5df38eac487b5e61891e94769a1
Y.Jung <gilsang9808@gmail.com>
2020-01-16 16:03:24 +0900

    compile error fix

--- plugins/multipath/path_schedulers/schedule_path_ecf.c
+++ plugins/multipath/path_schedulers/schedule_path_ecf.c
@@ -180,12 +180,12 @@ protoop_arg_t schedule_path_ecf(picoquic_cnx_t *cnx) {
             queue_t *reserved_frames = (queue_t *) get_cnx(cnx, AK_CNX_RESERVED_FRAMES, 0);
             uint64_t reserved_frame_len = (uint64_t) reserved_frames->size;
             uint64_t cwin_x = (uint64_t) get_path(path_x, AK_PATH_CWIN, 0);
-            uint64_t smoothed_rtt_c = (uint64_t) get_path(path_c, AK_PATH_SMOOTHED_RTT, 0);
+            // uint64_t smoothed_rtt_c = (uint64_t) get_path(path_c, AK_PATH_SMOOTHED_RTT, 0);
             // uint64_t tmp_rtt_x = (uint64_t) get_path(path_x, AK_PATH_SMOOTHED_RTT, 0);
                 
             if(
                 path_x && valid && 
-                reserved_frame_len / cwin_x * smoothed_rtt_x < smoothed_rtt_c - smoothed_rtt_x)
+                reserved_frame_len / cwin_x * smoothed_rtt_x < smoothed_rtt_c - smoothed_rtt_x
             ) {
                 continue;
             }
